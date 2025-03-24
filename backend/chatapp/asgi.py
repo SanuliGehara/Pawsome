ccb250990eb9473dfd14b639725c6bb70afb4abf
Sanjana-Sanjeevan <sanjanasanjeevan20@gmail.com>
2025-03-24 10:56:04 +0530

    Updated API Key & Pathway

--- backend/chatapp/asgi.py
+++ backend/chatapp/asgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import backend.Aichat.routing
+import Aichat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatapp.settings')
 
@@ -20,7 +20,7 @@ application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            backend.Aichat.routing.websocket_urlpatterns
+            Aichat.routing.websocket_urlpatterns
         )
     ),
 })
